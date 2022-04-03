e80a242cbf1dcd9455cb67b1e4ac541fb5fdb68d
Michael Hinz <michaelhinz.mh@googlemail.com>
2022-04-04 07:22:18 +1000

    Fixed singleton duplicating

--- Assets/_PROJECT/Scripts/DontDestroy.cs
+++ Assets/_PROJECT/Scripts/DontDestroy.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+
+    private static DontDestroy _instance;
+    
+    public static DontDestroy Instance
+    {
+        get { return _instance; }
+    }
+    
     // Start is called before the first frame update
     void Awake()
     {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+        
         DontDestroyOnLoad(gameObject);
     }
 }
